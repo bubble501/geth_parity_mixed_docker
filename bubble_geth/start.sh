6de22608d43c9d38181db562e80a3dc1738f2bc2
bubble601 <bubble601@gmail.com>
2017-08-09 18:46:04 +0800

    init commit

---
+++ bubble_geth/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+cd /
+geth init "/ethereum/geth.json"
+sleep 3
+geth --networkid 1337 --port 31333 --rpc --rpcapi "db,personal,eth,net,web3.miner" --rpccorsdomain "*" --rpcaddr "0.0.0.0" --rpcport 8538 --nodekey /ethereum/bootnode.key 
